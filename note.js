af69321546ff83d4edf1cb84130d4278a079317c
NayanSr <nayankbps@gmail.com>
2025-10-08 22:50:10 +0600

    initial setup

---
+++ note.js
@@ -0,0 +1,12 @@
+// frontend
+/* 
+npx create-vite frontend --template react
+npm i react-icons react-redux @reduxjs/toolkit react-router react-router-dom react-slick react-toastify slick-carousel
+
+ */ 
+
+// backend
+/* 
+npm init -y
+ npm i bcryptjs body-parser concurrently cookie-parser dotenv express jsonwebtoken mongoose multer nodemon
+*/
