1804da999cb2df50191f6dac0143c4fcfadea349
NayanSr <nayankbps@gmail.com>
2025-10-21 10:38:20 +0600

    image uploade to save in local folder using noad js backend

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import genreRoutes from './routes/genreRoutes.js';
 import moviesRoutes from './routes/moviesRoutes.js';
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 
 // Configuration
@@ -26,8 +27,12 @@ const PORT = process.env.PORT
 
 // Routes
 app.use("/api/v1/users", userRoutes);
-app.use('/api/v1/genre', genreRoutes)
-app.use('/api/v1/movies', moviesRoutes )
+app.use('/api/v1/genre', genreRoutes);
+app.use('/api/v1/movies', moviesRoutes);
+app.use("/api/v1/upload", uploadRoutes);
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
 
 app.listen(PORT,()=>console.log(`Listining from port ${PORT}`))
